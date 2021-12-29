fd2d450d6a0b0f50f0f3d46b36aa1552e9499839
Ana Beatriz <anabiawalker@gmail.com>
2021-12-29 17:42:22 -0300

    Adicionando Componentes Media, Soma e Sorteio

---
+++ src/components/Media.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+
+export default props =>{
+    return (
+        <Card title='Média dos Números' green>
+            <div>
+                <span>
+                    <span>Resulado:</span>
+                    <strong>5</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
