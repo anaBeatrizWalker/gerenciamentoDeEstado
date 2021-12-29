11c009341bd576fdb4e5cf1069374ae192161a24
Ana Beatriz <anabiawalker@gmail.com>
2021-12-29 17:26:42 -0300

    Adcicionando Componente Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux (Versão Simples)</h1>
+      <div className='linha'>
+        <Card title='Card 1' red>
+          wwww
+        </Card>
+      </div>
+
+      <div className='linha'>
+        <Card title='Card 2' green>
+          xxxx
+        </Card>
+      </div> 
+
+      <div className='linha'>
+        <Card title='Card 3' blue>
+          yyyy
+        </Card>
+      </div>
+
+      <div className='linha'>
+        <Card title='Card 4' purple>
+          zzzz
+        </Card>
+      </div>
     </div>
   );
 }
