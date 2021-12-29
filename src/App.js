3f19d1dc0b3f921b3e37d4237c04b883c8580597
Ana Beatriz <anabiawalker@gmail.com>
2021-12-29 17:35:07 -0300

    Adicionando Componente Intervalo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Versão Simples)</h1>
       <div className='linha'>
-        <Card title='Card 1' red>
-          wwww
-        </Card>
+        <Intervalo></Intervalo>
       </div>
 
       <div className='linha'>
         <Card title='Card 2' green>
           xxxx
         </Card>
-      </div> 
 
-      <div className='linha'>
         <Card title='Card 3' blue>
           yyyy
         </Card>
-      </div>
 
-      <div className='linha'>
         <Card title='Card 4' purple>
           zzzz
         </Card>
