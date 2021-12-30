6909616314a2b8399213f65eef44d1299cf4f249
Ana Beatriz <anabiawalker@gmail.com>
2021-12-30 11:21:23 -0300

    Refatorando as Actions Types

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,14 +1,16 @@
 //Actions Creators para números
+
+import {NUM_MIN_ALTERADO, NUM_MAX_ALTERADO} from './actionsTypes'
+
 export function alterarNumMin(novoNumero){
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
-
 export function alterarNumMax(novoNumero){
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
