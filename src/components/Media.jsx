46e987035a2e49668f1a911b967060005cce7f9d
Ana Beatriz <anabiawalker@gmail.com>
2021-12-30 10:11:41 -0300

    Adicionando Conexão com Redux

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Card from './Card'
 
-export default props =>{
+function Media(props){
+    
+    console.log(props.min)
+    console.log(props.max)
+
+    const {min, max} = props
 
     return (
         <Card title='Média dos Números' green>
             <div>
                 <span>
                     <span>Resulado:</span>
-                    <strong>{10}</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+//Função que mapeia algo que está no estado (inteiro) para dentro das propriedades do componente Media
+function mapStateToProps(state){
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max,        
+    }
+}
+export default connect(mapStateToProps)(Media)
