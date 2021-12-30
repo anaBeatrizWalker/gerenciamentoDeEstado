4edb27a8fea1386f24e6523b972903bb8bd1d802
Ana Beatriz <anabiawalker@gmail.com>
2021-12-30 11:14:18 -0300

    Refatorando Reducer

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,35 +1,8 @@
-import {createStore, combineReducers} from 'redux' //combina todos os reducers para gerar a store
+import {createStore, combineReducers} from 'redux'
+import numerosReducer from './reducers/numeros'
 
 const reducers = combineReducers({
-    numeros: function(state, action){//chave: nome do estado, valor:função que gera o estado da chave
-        switch(action.type){
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state,
-                    min: action.payload
-                }
-            case 'NUM_MAX_ALTERADO':
-                return {
-                    ...state,
-                    max: action.payload
-                }
-            default:
-                return {
-                    min: 1, //valores fixos
-                    max: 10
-                }
-        }
-
-    },
-    nomes: function(state, action){
-        console.log('Reducer nomes..')
-        console.log(state, ' ', action)
-        return [
-            'Ana',
-            'Gustavo',
-            'Pedro'
-        ]
-    }
+    numeros: numerosReducer,
 })
 
 //Função que configura e cria o estado a partir do reducer
